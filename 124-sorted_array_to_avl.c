c3c9793ba34e3eff825fcb281184a4a4cfaf4e2b
codebreaker55 <141238581+codebreaker55@users.noreply.github.com>
2024-03-29 00:28:39 +0200

    34. AVL - From sorted array

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -4,30 +4,35 @@ avl_t *sorted_array_to_avl(int *array, size_t size);
 avl_t *aux_sort(avl_t *parent, int *array, int begin, int last);
 
 /**
- * sorted_array_to_avl - Builds an AVL tree from an array.
+ * sorted_array_to_avl - a function that builds an AVL tree from an array.
  *
- * @array: A pointer to the first element of the array to be converted.
- * @size: The number of element in the array
+ * @array: is a pointer to the first element of the array to be converted.
+ * @size: is the number of element in the array
  *
- * Return: A pointer to the root node of the created AVL tree, NULL on failure.
- */
+ * Return: a pointer to the root node of the created AVL tree,
+ *	return NULL on failure
+*/
+
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
 	if (array == NULL || size == 0)
+	{
 		return (NULL);
+	}
 	return (aux_sort(NULL, array, 0, ((int)(size)) - 1));
 }
 
 /**
- * aux_sort - Create the tree using the half element of the array.
+ * aux_sort - a function that creates the tree using the half element of the array.
  *
- * @parent: Parent of the node to create.
- * @array: Sorted array.
- * @begin: Position where the array starts.
- * @last: Position where the array ends.
+ * @parent: is the parent of the node to create.
+ * @array: is a sorted array.
+ * @begin: is the position where the array starts.
+ * @last: is the position where the array ends.
  *
- * Return: Tree created.
- */
+ * Return: the tree created.
+*/
+
 avl_t *aux_sort(avl_t *parent, int *array, int begin, int last)
 {
 	avl_t *root;
@@ -39,7 +44,9 @@ avl_t *aux_sort(avl_t *parent, int *array, int begin, int last)
 		mid = (begin + last) / 2;
 		aux = binary_tree_node((binary_tree_t *)parent, array[mid]);
 		if (aux == NULL)
+		{
 			return (NULL);
+		}
 		root = (avl_t *)aux;
 		root->left = aux_sort(root, array, begin, mid - 1);
 		root->right = aux_sort(root, array, mid + 1, last);
