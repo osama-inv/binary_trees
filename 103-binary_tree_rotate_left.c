533bb2894943407e913d87af264026680b14c9b3
codebreaker55 <141238581+codebreaker55@users.noreply.github.com>
2024-03-28 17:03:54 +0200

    22. Rotate left

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -22,8 +22,11 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 	tmp = pivot->left;
 	pivot->left = tree;
 	tree->right = tmp;
+	
 	if (tmp != NULL)
+	{
 		tmp->parent = tree;
+	}
 	tmp = tree->parent;
 	tree->parent = pivot;
 	pivot->parent = tmp;
