302319268d365decb89ec68681118cb18db93b88
belal <belalismail252@gmail.com>
2024-03-28 16:28:48 -0700

    37. Heap - Insert

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -2,32 +2,6 @@
 
 heap_t *heap_insert(heap_t **root, int value);
 size_t binary_tree_size(const binary_tree_t *tree);
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-/**
- * binary_tree_node - a function that creates a binary tree node.
- *
- * @parent: a pointer to the parent of the node.
- * @value: is the value to be puted on the new node.
- *
- * Return: NULL if it fails, else return a pointer to the new node.
-*/
-
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new;
-
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
-
-	return (new);
-}
 /**
  * binary_tree_size - Measures the size of a binary tree.
  *
